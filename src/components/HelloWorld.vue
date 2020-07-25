693a4346f636de1d0745d43eec964fcc0ce27dd2
sunshine <weertz.joffrey@protonmail.ch>
2020-07-25 16:19:15 +0200

    Module# ajout vue routeur

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,9 +2,10 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      okkkk
+      This is how we deploy ur Vue application with ssl connexion 
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
+  </div>
 </template>
 
 <script>
